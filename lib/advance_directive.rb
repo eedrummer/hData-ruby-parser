1fcbfe79212e560b4ceb1cb74ca7eb183b14eb41
Andy Gregorowicz <andy@mitre.org>
2010-01-05 12:55:00 -0500

    Starting work on AdvanceDirective.to_xml

--- lib/advance_directive.rb
+++ lib/advance_directive.rb
@@ -1,9 +1,9 @@
 class AdvanceDirective < SectionBase
   section_namespace 'ad', 'http://projecthdata.org/hdata/schemas/2009/06/advance_directive'
   namespace 'core', 'http://projecthdata.org/hdata/schemas/2009/06/core'
-  
+
   attr_accessor :type, :free_text_type, :effective_date, :custodian_of_the_document, :status
-  
+
   def self.create_object(root)
     ad = AdvanceDirective.new
     ad.type = CodedValue.from_xml(root.at_xpath('ad:type'))
@@ -11,7 +11,15 @@ class AdvanceDirective < SectionBase
     ad.effective_date = DateRange.from_xml(root.at_xpath('ad:effectiveDate'))
     ad.custodian_of_the_document = Informant.from_xml(root.at_xpath('ad:custodianOfTheDocument'))
     ad.status = CodedValue.from_xml(root.at_xpath('ad:status'))
-        
+
     ad
+  end
+
+  def to_xml(builder)
+    builder ||= Builder::XmlMarkup.new(:indent => 2)
+
+    builder.advanceDirective(AdvanceDirective.namespace_hash) do |ad|
+      type.to_element(ad, :type) if type
+    end
   end
 end
